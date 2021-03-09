6ec50ddbcd53ea634085eee34b92157483c92453
Yuri Potawsky <potawsky.yuri@gmail.com>
2021-03-09 10:56:53 -0500

    3.9

--- Unit-4/example5.js
+++ Unit-4/example5.js
@@ -1,32 +1,11 @@
-/*
-   Copyright 2018 Makoto Consulting Group, Inc.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
 'use strict'
-// Some constants (Q: Move these someone else?)
 const PORT = 8080;
-// Location of the data directory
 const DATA_DIR = '../data';
-// The file name to be processed
 const FILE_NAME = "1mWords.txt";
-//
-// Wrap the file-processor in an HTTP server
 const http = require('http');
-//
 const logger = require('./simple-logger');
 const fileProcessor = require('./file-processor');
-//
+
 const server = http.createServer((request, response) => {
     // Request processor
     // Get the METHOD and URL from the request
@@ -78,3 +57,7 @@ var writeServerResponse = function(response, responseMessage, statusCode) {
     response.write(responseMessage);
     response.end();
 }
+
+console.log("Web Server Running at " + PORT);
+
+
